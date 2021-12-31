71f4e38eac346758776329cb20bf7e735e2295b6
Sebastian Heiden <tetje@yandex.ru>
2021-12-31 14:53:31 +0100

    update timing add writing to Geopackage to julia version

--- test_zonal_stat.jl
+++ test_zonal_stat.jl
@@ -4,6 +4,7 @@ using InteractiveUtils
 
 using GeoDataFrames; const GDF=GeoDataFrames
 using DataFrames
+using BenchmarkTools
 using Rasters
 using Statistics
 using GeoStats
@@ -14,46 +15,19 @@ md"""
 - filter villages in Wufeng/Taichung
 """
 
-startTime = now()
+
+@btime villages = GDF.read("./村里界圖20140313.json")
 
 villages = GDF.read("./村里界圖20140313.json")
 villages_taichung = villages[villages.COUNTYNAME .== "臺中市", :]
 villages_wufeng = villages_taichung[villages_taichung.TOWNNAME .== "霧峰區", :]
 
-md"""
-# Open DEM for that area
-"""
-
-dem_path = "./DEM/n24_e120_1arc_v3.tif"
-dem_model = Raster(dem_path)
-mean_hight_wufeng = map(x -> mean(skipmissing(replace_missing(mask(dem_model; to=x)))), villages_wufeng.geom)
-println((now() - startTime) / convert(Millisecond,Second(1)))
-
-md"""
-# Second Time
-"""
-
-startTime = now()
-localStartTime = startTime
-
-villages = GDF.read("./村里界圖20140313.json")
-villages_taichung = villages[villages.COUNTYNAME .== "臺中市", :]
-villages_wufeng = villages_taichung[villages_taichung.TOWNNAME .== "霧峰區", :]
-
-print("Loading and FIltering Esri Shape File took: ")
-println((now() - localStartTime) / convert(Millisecond,Second(1)))
-
 
 md"""
 # Open DEM for that area
 """
-localStartTime = now()
 dem_path = "./DEM/n24_e120_1arc_v3.tif"
-dem_model = Raster(dem_path)
-print("Opening the raster took: ")
-println((now() - localStartTime) / convert(Millisecond,Second(1)))
+@btime dem_model = read(Raster(dem_path))
+dem_model = read(Raster(dem_path))
 
-localStartTime = now()
-mean_height_wufeng = map(x -> mean(skipmissing(replace_missing(mask(dem_model; to=x)))), villages_wufeng.geom)
-print("Zonal stat took: ")
-println((now() - localStartTime) / convert(Millisecond,Second(1)))
+@btime mean_height_wufeng = map(x -> mean(skipmissing(replace_missing(mask(dem_model; to=x)))), villages_wufeng.geom)
