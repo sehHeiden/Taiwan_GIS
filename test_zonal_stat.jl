531d2c94fdbf5c6446b40cac83db82ccae5daed0
Sebastian Heiden <tetje@yandex.ru>
2022-01-02 14:27:02 +0100

    add mossaic

--- test_zonal_stat.jl
+++ test_zonal_stat.jl
@@ -7,16 +7,18 @@ using DataFrames
 using BenchmarkTools
 using Rasters
 using Statistics
-using GeoStats
-using Dates
 using ThreadTools
+
+println(" nthreads = ", Threads.nthreads())
+
+
 md"""
 # Open the vector geojson files for the villages in Taiwan
 - filter villages in Wufeng/Taichung
 """
 
 villages = GDF.read("./村里界圖20140313.json")
-@btime GDF.read("./村里界圖20140313.json")
+# @btime GDF.read("./村里界圖20140313.json")
 villages_taichung = villages[villages.COUNTYNAME .== "臺中市", :]
 villages_wufeng = villages_taichung[villages_taichung.TOWNNAME .== "霧峰區", :]
 
@@ -26,10 +28,15 @@ md"""
 """
 dem_path = "./DEM/n24_e120_1arc_v3.tif"
 dem_model = read(Raster(dem_path))
-@btime read(Raster(dem_path))
+# @btime read(Raster(dem_path))
+dem_path = "./DEM/n24_e120_1arc_v3.tif"
+dem_model = read(Raster(dem_path))
+dem_models = map(x->read(Raster(x)), filter(x->occursin("n24", x), readdir("./DEM"; join=true)))
 
-mean_height_wufeng = tmap(x -> mean(skipmissing(replace_missing(mask(dem_model; to=x)))), villages_wufeng.geom)
-@btime tmap(x -> mean(skipmissing(replace_missing(mask(dem_model; to=x)))), villages_wufeng.geom)
+dem_mosaic = mosaic(first, map(Raster, dem_models))
+
+ mean_height_wufeng = tmap(x -> mean(skipmissing(replace_missing(mask(dem_model; to=x)))), villages_wufeng.geom)
+ @btime tmap(x -> mean(skipmissing(replace_missing(mask(dem_model; to=x)))), villages_wufeng.geom)
 
 insertcols!(villages_wufeng, 1, :mean_heigth => mean_height_wufeng)
 GDF.write("wufeng.gpkg", villages_wufeng)
